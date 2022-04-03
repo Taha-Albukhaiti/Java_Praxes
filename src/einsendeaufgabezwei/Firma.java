601a2f9843559c24c0caffef8ded363f43579a37
Taha Al-Bukhaiti <taha.al-bukhaiti@stud.th-luebeck.de>
2022-04-03 22:43:59 +0200

    Neue Änderungen

--- src/einsendeaufgabezwei/Firma.java
+++ src/einsendeaufgabezwei/Firma.java
@@ -5,79 +5,93 @@ import java.io.FileReader;
 import java.io.IOException;
 
 /**
-  */
+ *
+ */
 public class Firma {
 
-	/* Maximale Anzahl der mit der Firma in Beziehung stehenden Personen
-	 * (Kund:innen oder Angestellte)
-	 */
-	private static final int MAX_ANZAHL = 10;
-	
-	/* Name der Firma 
-	 */
+    /* Maximale Anzahl der mit der Firma in Beziehung stehenden Personen
+     * (Kund:innen oder Angestellte)
+     */
+    private static final int MAX_ANZAHL = 10;
+
+    /* Name der Firma
+     */
     private String name;
-    
-	/* Array, das alle Kund:innen und Angestellten der Firma enthaelt
-	 */
-	private Person[]  allePersonen = new Person[MAX_ANZAHL];
+
+    /* Array, das alle Kund:innen und Angestellten der Firma enthaelt
+     */
+    private Person[] allePersonen = new Person[MAX_ANZAHL];
 
 
     /**
-      * Konstruktor
-      * 
-      * @param name Firmenname
-      * 
-      */
+     * Konstruktor
+     *
+     * @param name Firmenname
+     */
     public Firma(String name) {
-		
-		this.name = name;
+
+        this.name = name;
     }
 
 
-	/** Liest die Personendaten (Kund:innen und Angestellte) aus 
-	  * der als Parameter uebergebenen Datei, legt die entsprechenden 
-	  * Objekte an und traegt diese in das Array allePersonen ein.
-	  */
-	public void ladePersonen(String quelldatei) throws IOException{
+    /**
+     * Liest die Personendaten (Kund:innen und Angestellte) aus
+     * der als Parameter uebergebenen Datei, legt die entsprechenden
+     * Objekte an und traegt diese in das Array allePersonen ein.
+     */
+    public void ladePersonen(String quelldatei) throws IOException {
+        // ### to do ###
+        Kunde k;
+        allePersonen = null;
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(quelldatei));
+            String zeile = null;
+            while ((zeile = br.readLine()) != null) {
+                if (zeile.equals("k")) {
+                    zeile = null;
+                    while ((zeile = br.readLine()) != null) {
+                        String vor = zeile;
 
-		Kunde k;
-		allePersonen = null;
-		BufferedReader br = null;
-		try {
-            // ### to do ###
-			br = new BufferedReader(new FileReader("stammdaten.txt"));
-			while (br.ready()){
+                        k = new Kunde(zeile, );
+                    }
 
-				/*if(br == "k"){
-					kun = new Kunde;
-					kun.set
 
-				}*/
-			}
-			
-		} catch (Exception e) {
-			System.err.println("Fehler beim Laden: " + e.getMessage());
-		}
+                    System.out.println("Test: " + zeile);
+                }
+
+                //if (br.toString() == "k") {
+                //}
+            }
+
+        } catch (Exception e) {
+            System.err.println("Fehler beim Laden: " + e.getMessage());
+        } finally {
+            br.close();
+        }
+
     }
-    
 
-	/** Gibt alle Personen aus, die dem uebergebenen Filterkriterium entsprechen
-	  *
-	  * @param
-	  */
-	public void gibPersonenAus() {		// ### Parameter ergaenzen ###
 
-        // ### to do ###
-		
-	}
-	
-	/** Gibt alle Personen aus, die dem uebergebenen Filterkriterium entsprechen
-	  *
-	  * @param
-	  */
-	public void gibPersonenAusVariante2() {		// ### Parameter ergaenzen ###
+    /**
+     * Gibt alle Personen aus, die dem uebergebenen Filterkriterium entsprechen
+     *
+     * @param
+     */
+    public void gibPersonenAus() {        // ### Parameter ergaenzen ###
 
         // ### to do ###
-		
-	}
+
+    }
+
+    /**
+     * Gibt alle Personen aus, die dem uebergebenen Filterkriterium entsprechen
+     *
+     * @param
+     */
+    public void gibPersonenAusVariante2() {        // ### Parameter ergaenzen ###
+
+        // ### to do ###
+
+    }
 }
--- src/einsendeaufgabezwei/FirmaMain.java
+++ src/einsendeaufgabezwei/FirmaMain.java
@@ -1,22 +1,32 @@
 package einsendeaufgabezwei;
 
+import java.io.IOException;
+
 /**
-  */
+ *
+ */
 public class FirmaMain {
 
-	/* Der Name der Datei mit den Stammdaten 
-	 */
-	private static final String DATEINAME = "stammdaten.txt";
+    /* Der Name der Datei mit den Stammdaten
+     */
+    private static final String DATEINAME = "stammdaten.txt";
 
 
     /**
      * Main-Methode.
-     * 
+     *
      * @param args Uebergabeparameter
      */
     public static void main(String[] args) {
 
-		// ### to do ###
+        // ### to do ###
+
+        Firma m = new Firma("Blabal");
+        try {
+            m.ladePersonen(DATEINAME);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
